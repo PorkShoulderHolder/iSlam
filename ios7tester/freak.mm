f3cc2a76e63d0279edd1e79cd0dc04c863146681
Sam Royston <sfoxroyston@gmail.com>
2015-05-05 12:55:59 -0400

    images

--- ios7tester/freak.mm
+++ ios7tester/freak.mm
@@ -12,7 +12,7 @@
 
 cv::FREAK extractor;
 cv::Mat img_object,homography;
-cv::Mat descriptors_object, descriptors_scene;
+cv::Mat descriptors_object;
 std::vector<cv::KeyPoint> keypoints_object;
 float nndrRatio = 0.7f;
 
@@ -28,9 +28,8 @@ float nndrRatio = 0.7f;
         
         cv::Mat _temp = [self cvMatFromUIImage:self.templateImg];
         cv::cvtColor(_temp, img_object, CV_BGR2GRAY);
-        cv::FAST(img_object, keypoints_object, 7);
+        cv::FAST(img_object, keypoints_object, 9);
         extractor.compute( img_object, keypoints_object, descriptors_object);
-        
     }
     return self;
 }
@@ -61,16 +60,17 @@ float nndrRatio = 0.7f;
 
 - (void) processImage:(cv::Mat &)image{
     //cv::pyrDown(image, image);
+    
+    cv::Mat descriptors_scene;
 
     std::vector< cv::DMatch >   matches,good_matches;
 
     cv::Mat img_scene;
-    
     cv::cvtColor(image, img_scene, CV_BGR2GRAY);
-
     std::vector<cv::KeyPoint> keypoints_scene;
+    cv::FAST(img_scene, keypoints_scene, 9);
     
-    cv::FAST(img_scene, keypoints_scene, 7);
+    
 
     //-- Step 2: Calculate descriptors (feature vectors)
     
@@ -83,39 +83,59 @@ float nndrRatio = 0.7f;
     
     good_matches = [self processMatches:matches];
     
-    std::vector<cv::Point2f> dscene,dobject;
-    std::vector<cv::Point2f> scene;
-    std::vector<cv::Point2f>obj;
+    cv::vector<cv::Point2f> dscene,dobject;
+    cv::vector<cv::Point2f> scene;
+    cv::vector<cv::Point2f>obj;
     if (good_matches.size() > 4) {
-        for( int i = 0; i < good_matches.size(); i++ )
+        for( int i = 0; i < 20; i++ )
         {
             //-- Get the keypoints from the good matches
             obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
             scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
         }
         
-        cv::Mat homo = cv::findHomography(obj, scene, CV_RANSAC);
-        if (homography.empty()) {
-            homo.copyTo(homography);
-        }
-        cv::accumulateWeighted(homo, homography, 0.1);
+        cv::Mat trans = cv::findFundamentalMat(obj,scene);
 
+        
+//        cv::Mat homo = cv::findHomography(obj, scene, CV_RANSAC);
+//        if (homography.empty()) {
+//            homo.copyTo(homography);
+//        }
+//        cv::accumulateWeighted(homo, homography, 0.1);
+
+//        NSLog(@"rows: %i, columns: %i", homo.rows, homo.cols);
+//        for( int j = 0; j < homo.rows; j++){
+//            
+//            for (int h = 0; h < homo.cols; h++) {
+//                 NSLog(@"%f", homo.at<float>(j,h));
+//            }
+//        }
+        
         std::vector<cv::Point2f> obj_corners(4);
-        obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
-        obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
+        obj_corners[0] = cvPoint( img_object.cols / 2, img_object.rows / 2 );
+        obj_corners[1] = cvPoint( img_object.cols / 2 - 2, img_object.rows / 2 - 2 );
+        obj_corners[2] = cvPoint( img_object.cols / 2 - 2, img_object.rows / 2 + 2 );
+        obj_corners[3] = cvPoint( 0, img_object.rows );
         std::vector<cv::Point2f> scene_corners(4);
         
-        cv::perspectiveTransform( obj_corners, scene_corners, homography);
+        cv::perspectiveTransform( obj_corners, scene_corners, trans );
         
-        cv::line( img_scene, scene_corners[0] , scene_corners[1] , cv::Scalar(255, 255, 255), 4 );
-        cv::line( img_scene, scene_corners[1] , scene_corners[2] , cv::Scalar( 255, 255, 255), 4 );
-        cv::line( img_scene, scene_corners[2] , scene_corners[3] , cv::Scalar( 255, 255, 255), 4 );
-        cv::line( img_scene, scene_corners[3] , scene_corners[0], cv::Scalar( 255, 255, 255), 4 );
+        cv::line( img_scene, obj_corners[0] , scene_corners[0] , cv::Scalar(255, 255, 255), 2 );
+        cv::line( img_scene, scene_corners[0] , scene_corners[1] , cv::Scalar( 70, 0, 0), 5 );
+        cv::line( img_scene, scene_corners[0] , scene_corners[2] , cv::Scalar( 70, 0, 0), 5 );
+//      cv::line( img_scene, scene_corners[3] , scene_corners[0], cv::Scalar( 255, 255, 255), 4 );
     
     }
     
     cv::drawMatches( img_object, keypoints_object, img_scene, keypoints_scene, good_matches, image);
+    
+    keypoints_object = keypoints_scene;
+    descriptors_object = descriptors_scene;
+    img_object  = img_scene;
+
 }
+
+//- (void) drawMovement: (std::vector<<#class _Tp#>>)
 
 - (std::vector< cv::DMatch >)processMatches:(std::vector< cv::DMatch >)matches{
     
